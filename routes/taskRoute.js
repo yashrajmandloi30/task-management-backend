8090d9f6f43280993420e55342a117e8348209de
yashrajmandloi30 <yashmandloi32@gmail.com>
2026-05-23 16:40:59 +0530

    feat: enhance task creation and update logic with improved validation and email notifications

--- routes/taskRoute.js
+++ routes/taskRoute.js
@@ -11,12 +11,12 @@ const {
 } = require("../controller/taskController");
 const { isAdmin, authMiddleware } = require("../middleware/authMiddleware");
 
-router.post("/", isAdmin, createTask);
+router.post("/",authMiddleware,isAdmin, createTask);
 router.get("/", authMiddleware, getTasks);
 router.post("/reorder", authMiddleware, reorderTasks);
 router.get("/:id", authMiddleware, getTaskById);
-router.put("/:id", authMiddleware, updateTask);
+router.patch("/:id", authMiddleware,isAdmin , updateTask);
 router.patch("/:id/status", authMiddleware, updateTaskStatus);
-router.delete("/:id", isAdmin, deleteTask);
+router.delete("/:id" , authMiddleware, isAdmin, deleteTask);
 
 module.exports = router;
